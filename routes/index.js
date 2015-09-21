3622ae4fdd6f62afa2b6bb8df33fedc1beebd26e
hanangjie <hanangjie@gmail.com>
2015-09-21 17:52:40 +0800

    fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var conn=require("../server/config");
+var debug = require('debug')('vipadmin:index');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
@@ -10,9 +11,9 @@ router.get('/index', function(req, res, next) {
    try{
        var con=conn.conn();
        con.connect();
-        var sql="select * from customer where storeid='1' limit 10";
+        var sql="select * from customer where storeid='4' limit 10";
        con.query(sql,function(err1,res1){
-            console.log("error:"+err1+";result:"+res1);
+            debug("error:"+err1+";result:"+res1);
             push(res1);
         });
        function push(res1){
@@ -38,17 +39,16 @@ router.post('/regist', function(req, res, next) {
     try{
         var con=conn.conn();
         con.connect();
-        var time=new Date();
-        var mobile=parseInt(req.body.mobile);
+        var mobile=req.body.mobile&req.body.mobile.trim();
 
-        if(mobile!=req.body.mobile||mobile.toString().length>15){
+        if(mobile.toString().length>11){
             res.send({
                 code:-2,
                 msg:"手机格式不对"
             });
             return;
         }
-        var sql="insert into store(storename,mobile,pwd,time,status) values('"+req.body.name+"','"+mobile+"','"+req.body.pwd+"','"+time.getTime()+"','1')";
+        var sql="insert into store(storename,mobile,pwd,status) values('"+req.body.name+"','"+mobile+"','"+req.body.pwd+"','1')";
         con.query(sql,function(err1,res1){
             console.log("error:"+err1+";result:"+res1);
             if(res1.length>1) {
@@ -72,6 +72,34 @@ router.post('/regist', function(req, res, next) {
     }
 });
 
+router.post('/login', function(req, res, next) {
+    try{
+        var con=conn.conn();
+        con.connect();
+        var sql=`select * from store where mobile='${req.body.mobile}' and pwd='${req.body.pwd}'`;
+        con.query(sql,function(err1,res1){
+            debug("error:"+err1+";result:"+res1);
+            push(res1);
+        });
+        function push(res1){
+            if(res1.length>=1){
+                res.send({
+                    code:1,
+                    msg:"登录成功！"
+                });
+            }else{
+                res.send({
+                   code:-1,
+                    msg:"用户名或者密码失败"
+                });
+            }
+        }
+        con.end();
+
+    }catch(e){
+        console.log(e.stack);
+    }
+});
 
 
 module.exports = router;
